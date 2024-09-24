370ca98a101c7907986b0b22c4489a9af84b003d
princedonga <160412994+princedonga@users.noreply.github.com>
2024-09-24 10:18:29 +0530

    upload

--- src/Components/Preloader.js
+++ src/Components/Preloader.js
@@ -6,12 +6,19 @@ import { useEffect } from 'react';
 const Preloader = () => {
   useEffect(() => {
     Aos.init();
-}, []);
+  }, []);
   return (
     <div className='' >
-        <img data-aos="fade-down" data-aos-duration="3000" className='d-block mx-auto m' src='https://lh4.googleusercontent.com/proxy/ReEsB2qIN_qybP9R5TmMNxVrRzfs1RIfmv7ai6LOXE3elkI7_Vmmm_8oyVHcZMMgb5c2zKKeZt966TuSSBLuiZ0sYZyI5zw' alt='loader'></img>
-      <h2 data-aos="fade-left" data-aos-duration="3000" className='text-center text1'>SaltandGlitz</h2>
-     
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className=''>
+              <h2 className='text-center text1'>SaltandGlitz</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
